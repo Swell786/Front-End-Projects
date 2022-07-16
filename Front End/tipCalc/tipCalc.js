d7b17b1239c37bd52a73ece32efb71ed8443d553
Swell786 <106054900+Swell786@users.noreply.github.com>
2022-07-16 16:51:38 +0300

    Added Initial Projects

---
+++ Front End/tipCalc/tipCalc.js
@@ -0,0 +1,12 @@
+// Site Not Responsive
+
+//NOTE! if variables are declared out of function, button runs function once 
+
+function myFunc(){
+    var bill = document.getElementById("bill").value
+    var service = document.getElementById("service").value / 100
+    var shared = document.getElementById("billshare").value
+    
+    value = bill * service / shared
+    document.getElementById("tip").innerHTML = value.toFixed(2)
+}
