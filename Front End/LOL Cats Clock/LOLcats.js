b3798979513c6b1152072a7ebb2b04c07ee8011b
swell <suheilmiyanji@gmail.com>
2022-12-20 00:40:15 +0300

    animated body

--- Front End/LOL Cats Clock/LOLcats.js
+++ Front End/LOL Cats Clock/LOLcats.js
@@ -4,13 +4,17 @@
 
 // make body Animation
 
-var bodyColour = ['rgb(154,77,98)','rgb(154,27,198)']
-
-function changeBodyColour () {
-    for (i = 0 ; i < 3 ; i++) {
-    document.getElementsByTagName('body').style.color = bodyColour[i]
-}
-}
+bodyColours = [
+    "(rgb(249, 152, 141),rgb(230, 215, 169),rgb(249, 152, 141) 30%)",
+    "(rgb(230, 215, 169),rgb(249, 152, 141),rgb(230, 215, 169) 30%)",
+  ];
+  i = 0;
+  function changeBodyColour() {
+    document.getElementById("container").style.backgroundImage =
+    "repeating-linear-gradient" + bodyColours[i];
+    if (i >= 1) i = 0;
+    else i++;
+  }
 
 
 
